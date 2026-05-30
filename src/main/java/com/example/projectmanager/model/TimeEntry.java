efd345c31bc1d04d3599431f8d9badccf87f2ddb
vadimnekterov-wq <vadimnekterov@gmail.com>
2026-05-30 15:47:45 +0400

    Update TimeEntry.java

--- src/main/java/com/example/projectmanager/model/TimeEntry.java
+++ src/main/java/com/example/projectmanager/model/TimeEntry.java
@@ -14,7 +14,6 @@ public class TimeEntry {
     private LocalDateTime endTime;
     private String description;
 
-    public TimeEntry() {}
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public Long getTaskId() { return taskId; }
