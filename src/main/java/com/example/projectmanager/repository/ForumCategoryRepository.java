009038508b0ddf063a140b961b191e61c675b050
vadimnekterov-wq <vadimnekterov@gmail.com>
2026-05-30 13:59:07 +0400

    Update ForumCategoryRepository.java

--- src/main/java/com/example/projectmanager/repository/ForumCategoryRepository.java
+++ src/main/java/com/example/projectmanager/repository/ForumCategoryRepository.java
@@ -1,9 +1,13 @@
 package com.example.projectmanager.repository;
 
-import com.example.projectmanager.model.ForumCategory;
+import com.example.projectmanager.model.ForumPost;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
-public interface ForumCategoryRepository extends CrudRepository<ForumCategory, Long> {
-    List<ForumCategory> findByProjectId(Long projectId);
+public interface ForumPostRepository extends CrudRepository<ForumPost, Long> {
+
+    @Query("SELECT * FROM forum_posts WHERE topic_id = :topicId")
+    List<ForumPost> findByTopicId(@Param("topicId") Long topicId);
 }
